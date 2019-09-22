b4658e9e81725b1a066a1d412f27f3ef8d69be85
m <m@dontworry.com>
2019-09-22 10:19:47 -0400

    add COMPANYNAME variable

--- sed.sh
+++ sed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: $0 <your name> <your email> <your phone number>"
+  echo "Usage: $0 <your name> <your email> <your phone number> <company name>"
   exit 1
 fi
 
-sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/" index.html
+sed -i "s/YOURNAMEHERE/$1/ ; s/YOURGMAIL@gmail.com/$2/ ; s/5555555555/$3/ ; s/COMPANYNAME/$4/g" index.html
